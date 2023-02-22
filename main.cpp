5e8b1fbaf8edde192e72152ac94d855b1c14785c
Matti Vapa <matti.vapa@piimega.fi>
2023-02-22 11:21:56 +0200

    Add better threading model and bucketing
    This commit changes the threading model so that instead of assigning a
    list of pairs to process to each thread, we instead have one queue
    (a vector) for all pairs, and have each thread fetch pairs from that
    when they are done with processing the last one. We also no longer
    recurse a pair all the way down the tree of child pairs, but instead
    each thread adds all child pairs to the work queue so they can be
    processed separately.
    
    This commit also adds the option to split the work into "buckets" where
    the list of initial pairs is split into a number of units (buckets)
    that can be processed separately. The new CLI handles can be used to
    define the number of buckets to split the work into, and which bucket to
    process.
    
    The bucketing is done so that the initial pairs are spread around the
    buckets to prevent all sibling pairs from ending up in the same bucket.
    
    This, in theory, should spread the amount of work needed roughly evenly
    to each bucket.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <cassert>
 #include <cmath>
+#include <mutex>
+#include <functional>
+#include <chrono>
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 #include "fractions.hpp"
@@ -34,6 +37,47 @@ using BigInt = boost::multiprecision::int1024_t;
 using BigInt = NTL::ZZ;
 #endif
 
+std::mutex work_queue_mutex;
+std::mutex thread_status_mutex;
+
+int thread_count = 1;
+int done_thread_count = 0;
+
+struct configuration {
+    int N;
+    uint n_threads;
+    uint subdivisions;
+    uint buckets;
+    uint bucket;
+};
+
+// Very naive CLI argument parser
+configuration parse_cli_arguments(int argc, char* argv[]) {
+    configuration config = {10, 1, 0, 1, 1};
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            auto argument = std::string(argv[i]);
+            auto handle = argument.substr(0,2);
+            if (handle == "-N") {
+                config.N = std::stoi(argument.substr(2));
+            } else if (handle == "-j") {
+                config.n_threads = std::stoi(argument.substr(2));
+            } else if (handle == "-s") {
+                config.subdivisions = std::stoi(argument.substr(2));
+            } else if (handle == "-B") {
+                config.buckets = std::stoi(argument.substr(2));
+            } else if (handle == "-b") {
+                config.bucket = std::stoi(argument.substr(2));
+            }
+        }
+    }
+
+    assert(config.n_threads <= std::thread::hardware_concurrency());
+    assert(config.N > 0);
+    assert(config.buckets >= 1);
+    assert(config.bucket > 0 && config.bucket <= config.buckets);
+    return config;
+}
 
 template <typename T>
 void check_pair(const fractions::convergent_pair<T>& pair, int depth, int N) {
@@ -54,37 +98,101 @@ void check_pair(const fractions::convergent_pair<T>& pair, int depth, int N) {
     }
 }
 
+bool all_threads_done() {
+    thread_status_mutex.lock();
+    bool all_done = thread_count == done_thread_count;
+    thread_status_mutex.unlock();
+    return all_done;
+}
+
+void increment_done_thread_count() {
+    thread_status_mutex.lock();
+    done_thread_count++;
+    thread_status_mutex.unlock();
+}
+
+void decrement_done_thread_count() {
+    thread_status_mutex.lock();
+    done_thread_count--;
+    thread_status_mutex.unlock();
+}
+
 template <typename T>
-void check_pair_list(const std::span<fractions::convergent_pair<T>>& pairs, int N) {
-    for (auto& pair: pairs) {
-        check_pair(pair, 0, N);
+void process(std::vector<fractions::convergent_pair<T>>& queue, int N) {
+    
+    bool done = false;
+
+    // Until all threads have marked themselves as done, we need to try processing pairs from the queue.
+    while (!all_threads_done()) {
+        
+        work_queue_mutex.lock();
+        // If there is work available
+        if (queue.size() > 0) {
+            // If we previously marked ourself to be done:
+            if (done) {
+                // Decrease the count of done threads by one
+                decrement_done_thread_count();
+                // and mark ourself as not done.
+                done = false;
+            }
+
+            // Get a new pair to work on and unlock the mutex.
+            auto pair = std::move(queue.back());
+            queue.pop_back();
+            work_queue_mutex.unlock();
+
+            // The pair passes the criteria, so we can forget about it and jump back to the start of the loop.
+            if (LW::meets_littlewood_criteria(pair, N)) {
+                continue;
+            }
+
+            #if defined(FIXED_WIDTH_INTEGERS) && defined(OVERFLOW_PROTECTION)
+            // If we are using fixed width integers, this check should guarantee that we can't overflow the
+            // integer in the next iteration.
+            assert(boost::multiprecision::pow(pair.alpha.current.den, 5) < boost::math::tools::max_value<BigInt>() / (static_cast<BigInt>(2 * std::pow(N, 8))));
+            #endif
+
+            // The pair does not match the criteria, so we divide it into N new pairs with larger denominators
+            std::vector<fractions::convergent_pair<T>> child_pairs = {};
+            fractions::subdivide(pair, N, child_pairs);
+            // and add them to the work queue.
+            work_queue_mutex.lock();
+            queue.insert(std::end(queue), std::begin(child_pairs), std::end(child_pairs));
+            work_queue_mutex.unlock();
+        } else {
+            // No work available on the queue
+            work_queue_mutex.unlock();
+            // If we have not yet marked ourself as done:
+            if (!done) {
+                // Mark ourself as done
+                done = true;
+                // and increment the number of done threads.
+                increment_done_thread_count();
+            }
+            // Sleep for 2 seconds and before continuing the while loop.
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(2000ms);
+        }
     }
 }
 
-struct configuration {
-    int N;
-    uint n_threads;
-    uint subdivisions;
-};
+template <typename T>
+std::vector<fractions::convergent_pair<T>> select_bucket(std::vector<fractions::convergent_pair<T>>& pairs, configuration config) {
+    assert(pairs.size() >= config.buckets);
 
-configuration parse_cli_arguments(int argc, char* argv[]) {
-    configuration config = {10, 1, 0};
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            auto argument = std::string(argv[i]);
-            if (argument.substr(0,2) == "-N") {
-                config.N = std::stoi(argument.substr(2));
-            } else if (argument.substr(0,2) == "-j") {
-                config.n_threads = std::stoi(argument.substr(2));
-            } else if (argument.substr(0,2) == "-s") {
-                config.subdivisions = std::stoi(argument.substr(2));
-            }
-        }
+    if (config.buckets == 1) {
+        return pairs;
     }
 
-    assert(config.n_threads <= std::thread::hardware_concurrency());
-    assert(config.N > 0);
-    return config;
+    std::vector<fractions::convergent_pair<T>> bucket = {};
+
+    int index = config.bucket - 1;
+    while (index < pairs.size()) {
+        bucket.push_back(pairs[index]);
+        index += config.buckets;
+    }
+
+    return bucket;
 }
 
 int main(int argc, char* argv[]) {
@@ -104,29 +212,18 @@ int main(int argc, char* argv[]) {
     auto pairs = fractions::convergent_pairs<BigInt>(config.N, config.subdivisions);
     std::cout << fmt::format("Initial pairs (from {} subdivisions): {}", config.subdivisions, pairs.size()) << std::endl;
 
-    // Shuffle the pairs so that the different sized numerators are evenly split among the
-    // threads. Otherwise one thread might end up processing all the pathological ones, and we
-    // lose a lot of the value from multithreading.
-    auto rd = std::random_device {};
-    auto rng = std::default_random_engine {rd()};
-    std::shuffle(std::begin(pairs), std::end(pairs), rng);
+    auto bucket = select_bucket<BigInt>(pairs, config);
+    std::cout << fmt::format("Pairs split into {} buckets, processing bucket #{} which has {} pairs.", config.buckets, config.bucket, bucket.size()) << std::endl;
+
+    thread_count = config.n_threads;
 
     std::vector<std::thread> threads;
-
-    int pairs_per_thread = pairs.size() / config.n_threads;
-
-    // If the number of pairs is not neatly divisible by the number of threads
-    // the last thread will get the extra pairs, as calculated here.
-    int rest = pairs.size() - (config.n_threads - 1) * pairs_per_thread;
-    
     for (uint i = 0; i < config.n_threads; i++) {
-        int start = i * pairs_per_thread;
-        int number_of_pairs_to_process = i < config.n_threads ? pairs_per_thread : rest;
-        threads.emplace_back(
-            check_pair_list<BigInt>,
-            std::span<fractions::convergent_pair<BigInt>>{pairs}.subspan(start, number_of_pairs_to_process),
-            config.N
-        );
+            threads.emplace_back(
+                process<BigInt>,
+                std::ref(bucket),
+                config.N
+            );
     }
 
     for (std::size_t i = 0; i < threads.size(); i++) {
