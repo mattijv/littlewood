5e5fff94763fac36b55593ba42c13d8b09f3e66c
Matti Vapa <matti.vapa@piimega.fi>
2023-04-09 19:32:35 +0300

    Add new visualisation methods

---
+++ visualisation.hpp
@@ -0,0 +1,25 @@
+#ifndef VISUALISATION
+#define VISUALISATION
+
+#include <sstream>
+#include "fractions.hpp"
+
+namespace visualisation {
+
+    template <typename T>
+    std::stringstream string_representation(const fractions::convergent_pair<T>& pair) {
+        std::stringstream output;
+        output << "{";
+        output << pair.alpha.current.num << "/" << pair.alpha.current.den;
+        output << ", ";
+        output << pair.alpha.previous.num << "/" << pair.alpha.previous.den;
+        output << "}, {";
+        output << pair.beta.current.num << "/" << pair.beta.current.den;
+        output << ", ";
+        output << pair.beta.previous.num << "/" << pair.beta.previous.den;
+        output << "}";
+        return output;
+    }
+}
+
+#endif
